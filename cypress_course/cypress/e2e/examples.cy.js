e48683f65db7dd23b6ed5b5d9344f5d53bb223d7
Amilcar <amilcarjh24@gmail.com>
2024-01-22 21:15:13 -0300

    Add example.cy file and tests

---
+++ cypress_course/cypress/e2e/examples.cy.js
@@ -0,0 +1,36 @@
+describe("Various examples", () => {
+  beforeEach(() => {
+    cy.visit("/examples");
+  });
+
+  it("multi-page teting", () => {
+    cy.getDataTest("nav-why-cypress").click();
+    cy.location("pathname").should("equal", "/");
+
+    cy.getDataTest("nav-overview").click();
+    cy.location("pathname").should("equal", "/overview");
+
+    cy.getDataTest("nav-fundamentals").click();
+    cy.location("pathname").should("equal", "/fundamentals");
+
+    cy.getDataTest("nav-forms").click();
+    cy.location("pathname").should("equal", "/forms");
+
+    cy.getDataTest("nav-examples").click();
+    cy.location("pathname").should("equal", "/examples");
+
+    cy.getDataTest("nav-component").click();
+    cy.location("pathname").should("equal", "/component");
+
+    cy.getDataTest("nav-best-practices").click();
+    cy.location("pathname").should("equal", "/best-practices");
+  });
+
+  it.only("Intercepts", () => {
+    cy.intercept("POST", "http://localhost:3000/examples", {
+      fixture: "example.json",
+    });
+
+    cy.getDataTest("post-button").click();
+  });
+});
